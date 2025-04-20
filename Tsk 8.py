e50e0fb713b214dd1b0e53aff7a543e5a0436b07
ka-ponasenko <Ponasenko.ekatena@mail.ru>
2025-04-20 21:26:49 +0700

    Update Tsk 8.py

--- Tsk 8.py
+++ Tsk 8.py
@@ -2,7 +2,6 @@ def get_digit(position):
     sequence = "0"
     for number in range(1, 201):
         sequence += str(number)
-    # Возвращаем цифру по указанному порядковому номеру
     return sequence[position - 1]
 
 position = int(input("Введите порядковый номер: "))
